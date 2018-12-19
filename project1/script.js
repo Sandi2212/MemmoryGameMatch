be973cba8f4b54a17df6f9ea810f69adac313de9
Sandi <Sandiimayeguahi@gmail.com>
2018-12-18 21:52:34 -0500

    slightly better

--- project1/script.js
+++ project1/script.js
@@ -17,35 +17,83 @@ const squares = [
 ];
 
 let squaresArray = [
-{ 
-	name:'cat1', img: 'images/cat.JPEG', type: 'princes'
-},
-{
-	name:'cat2', img: 'images/cat2.JPEG', type: 'princes'
-},
-{
-	name:'mmouse', img: 'images/mic.JPEG', type: 'princes'
-},
-{ name:'mmouse2', img: 'images/mic2.JPEG', type: 'princes'},
-{ name:'princess1', img: 'images/jerry.JPEG', type:'princes'},
-{ name:'princess', img: 'images/jerry2.JPEG', type: 'princes'},
-{ name:'dog', img:'images/dog.JPEG', type: 'princes'},
-{ name:'dog2', img: 'images/dog2.JPEG', type: 'princes'}
+	{ 
+		name:'cat1',
+		img: './images/cat.jpeg', 
+		type: 'princes1'
+	},
+	{
+		name:'cat2',
+		img: './images/cat2.jpeg',
+		type: 'princes2'
+	},
+	{
+		name:'mmouse', 
+		img: './images/mic.jpeg', 
+		type: 'princes'
+	},
+	{
+		name:'mmouse2', 
+		img: './images/mic2.jpeg', 
+		type: 'princes'
+	},
+	{ 	
+		name:'princess1', 
+		img: './images/jerry.jpeg', 
+		type:'princes'
+	},
+	{ 	name:'princess', 
+		img: './images/jerry2.jpeg', 
+		type: 'princes'
+	},
+	{
+		name:'dog', 
+		img:'./images/dog.jpeg', 
+		type: 'princes'
+	},
+	{ 	name:'dog2', 
+		img: './images/dog2.jpeg', 
+		type: 'princes'
+	}
 ];
 
 function renderSquares(){
 	for (let square of squaresArray){
 		let cardDiv = document.createElement('div');
 		cardDiv.classList.add('square');
+		// cardDiv.classList.add('front');
 
 		let cardImg = document.createElement('img');
+		cardImg.classList.add('card-image');
 		cardImg.setAttribute('src', `${square.img}`);
+		cardImg.setAttribute('data-type', `${square.type}`);
+//data variable is a useful tool for comparison in this game
+		cardDiv.appendChild(cardImg);
+		cardDiv.addEventListener('click', (evt) => {
+			handleClick(evt);
+		});
+		
+
+		// let cardDiv2 = document.createElement('div');
+		// cardDiv2.classList.add('square');
+		// cardDiv2.classList.add('back');
+
+		// let cardImg2 = document.createElement('img');
+		// cardImg2.classList.add('card-image');
+
+		// cardImg.setAttribute('src', './images/dis4.jpeg');
+
+		// console.log(cardDiv);
+		// console.log(cardImg);
 
 		// cardDiv.style.top = (square.y * 198).toString() + 'px';
 		// cardDiv.style.left = (square.x * 198).toString() + 'px';
 
 		//create div for each square in gameboard
-		cardDiv.appendChild(cardImg);
+
+		// cardDiv.appendChild(cardDiv2);
+		// cardDiv.appendChild(cardImg2);
+		// cardDiv.appendChild(cardImg);
 		
 		//attach the event listener here!
 		// cardDiv.addEventListener("click" , (e) => {
@@ -59,18 +107,48 @@ function renderSquares(){
 		$gameboard.append(cardDiv);	
 	}	
 }
+	// function showCard() {
+	// 		// body...
+	// 		 cardDiv.addEventListener ('click', () => {
+	// 	 	cardDiv.style.src = squaresArray.length;
+	// 	 })
+	// 		console.log(showCard); 
+
+	// 	}
 
 renderSquares();
+
+let clickArr = [];
+
+function handleClick(evt) {
+	let stop = false;
+	// After 2nd click
+	while (clickArr.length < 3 && stop === false) {
+		if (clickArr.length < 2) {
+		 	clickArr.push(evt);
+		 	console.log(evt.target.dataset.type);
+
+		 	// check if they match
+		 }
+		 // After 1st click
+		 else if (clickArr.length < 1) {
+		 	clickArr.push(evt);
+		 	console.log(evt.target.dataset.type);
+		 }
+		 stop = true;
+	}
+}
+	
 
 
 ///need to get all thenmeory cards
 
-function flip() {
+// function flip() {
 
-	//if the card has not been clicked then click it
-	//if it been clicked the
+// 	//if the card has not been clicked then click it
+// 	//if it been clicked the
 	
-}
+// }
 //use an array of objects for name/images/type=="match cards" 
 
 //will need a shuffle fuinction
