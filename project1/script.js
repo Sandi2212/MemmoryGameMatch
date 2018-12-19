a3cb38e147abddd3b575f868a95b7e80d42d86fb
Sandi <Sandiimayeguahi@gmail.com>
2018-12-19 13:21:44 -0500

    got match function and shuffle functionality

--- project1/script.js
+++ project1/script.js
@@ -1,6 +1,6 @@
 const $gameboard= document.querySelector('.game-board');
-const deck = []
-let clickC = 1
+
+
 const squares = [
   { x: 0, y: 0 },  		
   { x: 0, y: 1 },		
@@ -23,7 +23,7 @@ let squaresArray = [
 		type: 'princes1'
 	},
 	{
-		name:'cat2',
+		name:'cat1',
 		img: './images/cat2.jpeg',
 		type: 'princes2'
 	},
@@ -33,16 +33,16 @@ let squaresArray = [
 		type: 'princes'
 	},
 	{
-		name:'mmouse2', 
+		name:'mmouse', 
 		img: './images/mic2.jpeg', 
 		type: 'princes'
 	},
 	{ 	
-		name:'princess1', 
+		name:'jerry', 
 		img: './images/jerry.jpeg', 
 		type:'princes'
 	},
-	{ 	name:'princess', 
+	{ 	name:'jerry', 
 		img: './images/jerry2.jpeg', 
 		type: 'princes'
 	},
@@ -51,70 +51,51 @@ let squaresArray = [
 		img:'./images/dog.jpeg', 
 		type: 'princes'
 	},
-	{ 	name:'dog2', 
+	{ 	name:'dog', 
 		img: './images/dog2.jpeg', 
 		type: 'princes'
 	}
 ];
 
+let usedCards = []
+// cardDiv.setAttribute("src", `{square[cardIdCheck].img}`);
+// 	 cardIdCheck = this.getAttribute('data-name');
 function renderSquares(){
-	for (let square of squaresArray){
+	for (var i = squaresArray.length -1 ; i >= 0; i--) {
+		//had to use a decrementing for loop because originally when I used the splice method to 
+		// remove items from beeing repeated more than once
+		// This is because iterating incrementally through the array, when you splice it, 
+		// the array is modified in place, so the items are 
+		// "shifted" and you end up skipping the iteration of some. 
+		// Looping backwards (with a while or even a for loop) fixes 
+		// this because you're not looping in the direction you're splicing. 
+		console.log(i)
+		console.log(squaresArray.length)
 		let cardDiv = document.createElement('div');
 		cardDiv.classList.add('square');
 		// cardDiv.classList.add('front');
 
 		let cardImg = document.createElement('img');
 		cardImg.classList.add('card-image');
-		cardImg.setAttribute('src', `${square.img}`);
-		cardImg.setAttribute('data-type', `${square.type}`);
-//data variable is a useful tool for comparison in this game
+		let number = Math.floor(Math.random() * squaresArray.length);
+		cardImg.setAttribute('src', `${squaresArray[number].img}`);
+		cardImg.setAttribute('data-name', `${squaresArray[number].name}`);
+	 	let randomArray = squaresArray.splice(number, 1)
+	 	 console.log(randomArray);
+	 	 
+
+//data attribute is a useful tool for comparison in this game
 		cardDiv.appendChild(cardImg);
 		cardDiv.addEventListener('click', (evt) => {
 			handleClick(evt);
+
 		});
 		
 
-		// let cardDiv2 = document.createElement('div');
-		// cardDiv2.classList.add('square');
-		// cardDiv2.classList.add('back');
-
-		// let cardImg2 = document.createElement('img');
-		// cardImg2.classList.add('card-image');
-
-		// cardImg.setAttribute('src', './images/dis4.jpeg');
-
-		// console.log(cardDiv);
-		// console.log(cardImg);
-
-		// cardDiv.style.top = (square.y * 198).toString() + 'px';
-		// cardDiv.style.left = (square.x * 198).toString() + 'px';
-
-		//create div for each square in gameboard
-
-		// cardDiv.appendChild(cardDiv2);
-		// cardDiv.appendChild(cardImg2);
-		// cardDiv.appendChild(cardImg);
-		
-		//attach the event listener here!
-		// cardDiv.addEventListener("click" , (e) => {
-		// 	if(e.target === cardImg) {
-		// 	 //if there is not any element in the box then create one/// img been clicked
-		// 	 console.log('clicked');
-		// 	}
-			
-		// });
 		
 		$gameboard.append(cardDiv);	
-	}	
-}
-	// function showCard() {
-	// 		// body...
-	// 		 cardDiv.addEventListener ('click', () => {
-	// 	 	cardDiv.style.src = squaresArray.length;
-	// 	 })
-	// 		console.log(showCard); 
+	}}
 
-	// 	}
 
 renderSquares();
 
@@ -125,25 +106,55 @@ function handleClick(evt) {
 	// After 2nd click
 	while (clickArr.length < 3 && stop === false) {
 		if (clickArr.length < 2) {
-		 	clickArr.push(evt);
-		 	console.log(evt.target.dataset.type);
+		 	clickArr.push(evt.target.dataset.name);
+		 	console.log(clickArr)
+		 	// console.log(evt.target.dataset.name);
+		 	checkForMatch(evt.target.dataset.name);
 
 		 	// check if they match
 		 }
 		 // After 1st click
 		 else if (clickArr.length < 1) {
 		 	clickArr.push(evt);
-		 	console.log(evt.target.dataset.type);
+		 	console.log(evt.target.dataset.name);
+		 	
 		 }
 		 stop = true;
 	}
 }
 	
 
+function checkForMatch (evt){
+
+
+	// console.log(evt);
+	
+	if(clickArr.length === 2 ){
+
+	let firstCard = clickArr[0].length;
+	let secondCard = clickArr[1].length;
+
+	console.log(firstCard, "firstcard");
+	console.log(secondCard, "2card");
+
+	if (firstCard  === secondCard) {
+		alert("it's a match")
+	} else {
+		alert("not a match")
+	}
+
+	
+		
+}
+}
+
+	
+ 
+
 
 ///need to get all thenmeory cards
 
-// function flip() {
+
 
 // 	//if the card has not been clicked then click it
 // 	//if it been clicked the
